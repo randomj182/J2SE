12215a09f2b69300987ab3ef0dc0338846052534
randomj182 <983851677@qq.com>
2018-09-21 17:14:27 +0800

    commit

--- src/main/java/com/random/concurrent/T.java
+++ src/main/java/com/random/concurrent/T.java
@@ -9,12 +9,19 @@ package com.random.concurrent;
 public class T implements Runnable {
 
 	private int count = 10;
-	
+	/**
+	 * https://www.bilibili.com/video/av11076511/?p=1  16:00
+	 * 有synchronized的时候,多个线程间run方法都不会重入,不会线程重入 原子操作 不可分
+	 */
 	public /*synchronized*/ void run() { 
 		count--;
 		System.out.println(Thread.currentThread().getName() + " count = " + count);
 	}
-	
+	/*
+	 * 此处只new了一个对象,好多线程共用这个对象
+	 * 
+	 * 5个线程访问的都是同一个count
+	 */
 	public static void main(String[] args) {
 		T t = new T();
 		for(int i=0; i<5; i++) {
